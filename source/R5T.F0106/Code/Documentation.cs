baf609ea6ff38a66955f051ab14dcbfc828a0c71
davidcoats <david@getsafetycone.com>
2023-02-27 14:46:45 -0800

    Initial commit.

---
+++ source/R5T.F0106/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0106
+{
+	/// <summary>
+	/// Member name parsing and conversion functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0106/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0106
+Member name parsing and conversion functionality.
